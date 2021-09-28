347b36f907a3cfeaaf00d4b3a9b7dddbb147b945
keyneskanno <keyneskanno@gmail.com>
2021-09-28 15:37:36 -0300

    estudando composer

---
+++ src/Buscador.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace UFU\BuscadorDeCursos;
+
+use GuzzleHttp\ClientInterface;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Buscador
+{
+
+    private $crawler;
+
+    public function __construct(ClientInterface $httpClient, Crawler $crawler)
+    {
+        
+    }
+}
---
+++ src/busca-cursos.php
@@ -0,0 +1,23 @@
+<?php
+
+require '../vendor/autoload.php';
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+$client = new \GuzzleHttp\Client();
+$resposta = $client->request('GET', 'http://www.comunica.ufu.br/noticias/');
+$html = $resposta->getBody();
+
+$crawler = new Crawler();
+$crawler->addHtmlContent($html);
+$cursos = $crawler->filter('div.field-name-field-linha-fina');
+
+
+
+foreach($cursos as $curso){
+    echo $curso->textContent . PHP_EOL;
+    
+    
+}
+
